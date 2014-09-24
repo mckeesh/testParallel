56f03bf3828937657c50bf59e9066193b825f139
Shane McKee <mckeesh@onid.oregonstate.edu>
2014-09-24 11:48:33 -0700

    initial commit; nothing works

---
+++ sum.js
@@ -0,0 +1,75 @@
+var SERIES_END = 20000;
+var mapFunction = function(x){
+    $.get("http://www.google.com");
+}
+
+var getTimestamp = function() { return window.performance.now(); };
+
+var getSeriesOfNumbers = function(begin, end){
+    var numList = [];
+    
+    for (var i = begin; i <= end; i++){
+	numList.push(i);
+    }
+
+    return numList;
+};
+
+var findSumOfArrayMapReduce = function(array){
+   
+    return array.map(function(element){
+	mapFunction(element);})
+};
+
+var findSumOfArray = function(array){
+     var total = 0;
+    for (var i = 0; i < array.length; i++){
+	mapFunction();
+    }
+    return total;
+
+}
+
+var addSumToDiv = function(sumFunction){
+    var begin = 1;
+    var end = SERIES_END;
+    var sum = 0;
+    var series = getSeriesOfNumbers(begin, end);
+    
+    var start = getTimestamp();
+
+    var sum = sumFunction(series);
+    
+    var elapsed = getTimestamp() - start;
+    
+    console.log(elapsed);
+    var div = document.getElementById('sequentialSum');
+    div.innerHTML += " Non parallel Took:" + elapsed + "ms <br/>";
+};
+
+var addSumToDivParallel = function(){
+    var begin = 1;
+    var end = SERIES_END;
+    var sumPar = 0;
+    var series = getSeriesOfNumbers(begin, end);
+    var start = getTimestamp();
+    
+    var sumPar = series.mapPar(function(element){
+	mapFunction();
+    });
+
+
+    var elapsed = getTimestamp() - start;
+
+    var div = document.getElementById('parallelSum');
+    div.innerHTML = "Parallel took : "+ elapsed + "ms";
+};
+
+var runEverything = function(){
+    addSumToDiv(findSumOfArray);
+    addSumToDiv(findSumOfArrayMapReduce);
+    addSumToDivParallel();
+
+};
+
+window.onload = runEverything;
